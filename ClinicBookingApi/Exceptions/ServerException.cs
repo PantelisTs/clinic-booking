0bb2957e16fb426c7bb78d6214d9a70aae7bbf32
PantelisTs <pantsotsgr@gmail.com>
2026-08-19 01:07:58 +0300

    feat: add custom exception hierarchy for structured error handling

---
+++ ClinicBookingApi/Exceptions/ServerException.cs
@@ -0,0 +1,10 @@
+﻿namespace ClinicBookingApi.Exceptions
+{
+	public class ServerException : AppException
+	{
+		public ServerException(string code, string message)
+			: base(code, message)
+		{
+		}
+	}
+}
