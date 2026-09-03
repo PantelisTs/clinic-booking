25726c66d4f45361a22076fbdb148a1f363432bb
PantelisTs <pantsotsgr@gmail.com>
2026-09-03 06:05:32 +0300

    feat: add Tailwind CSS v4.3 configuration

--- clinicbooking-frontend/src/App.tsx
+++ clinicbooking-frontend/src/App.tsx
@@ -5,7 +5,9 @@ function App() {
 
   return (
     <>
-      <h1>Clinic Booking</h1>
+      <h1 className="text-3xl font-bold text-blue-600 text-center mt-10">
+          Clinic Booking
+      </h1>
     </>
   )
 }
